4a9e7b65fdec715d70412795e3b9c834ec64491f
Hawker <62062830+BlckHawker@users.noreply.github.com>
2024-07-04 14:56:30 -0400

    tweak font size

---
+++ Assets/Purgatorys Name List/Script/Button.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Button  {
+    public KMSelectable Selectable { get; private set; }
+    public Text Text { get; private set; }
+    public Button(KMSelectable selectable, Text text)
+    {
+        Selectable = selectable;
+        Text = text;
+    }
+}
